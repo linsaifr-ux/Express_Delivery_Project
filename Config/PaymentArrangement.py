0caa79589b5ef294cf835da2b9bbf20f9becfe27
Samuel Lai <lai.sz.jen@gmail.com>
2025-12-10 22:28:10 +0800

    Add PaymentArrangement Class

---
+++ Config/PaymentArrangement.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 10 21:45:31 2025
+
+@author: laisz
+"""
+from enum import Enum
+
+
+class PaymentArrangement(Enum):
+    from_account = 0
+    cash = 1
+    mobile_wallet = 2
+    
+    
+    
+
+
