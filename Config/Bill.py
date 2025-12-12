e45d0aa99ad8d5f004efc61c6a7bec9906dde392
Samuel Lai <lai.sz.jen@gmail.com>
2025-12-12 18:16:18 +0800

    Finish Bill class

--- Config/Bill.py
+++ Config/Bill.py
@@ -5,17 +5,22 @@ Created on Wed Dec 10 21:26:22 2025
 
 @author: laisz
 """
-from PaymentArrangement import PaymentArrangement
+from PaymentRecord import PaymentRecord
+from PaymentArrangement import PaymentMethod
 
-class Bill:
+def verify_transaction(transaction_ID, amount) -> bool:
+    "A mock up for verifying a transaction."
+    return True
+
+class Bill:        
     # NameError resolved
     def __init__(self, outer_ref: Customer, ID: int, payment_amount: float,
                   order_ID: str):
         self.outer = outer_ref
         self.__ID = "B" + str(self.outer.ID)[1:] + f"{self.outer.bill_cnt: 04d}"
         self.__amount = payment_amount
-        self.payment_arrangement = None
         self.__payment_status = False
+        self.__payment_record = None
         self.__manifest = [order_ID]
     
     @property
@@ -27,21 +32,29 @@ class Bill:
         return self.__amount
         
     @property
-    def payment_arrangement(self) -> PaymentArrangement:
-        return self.__payment_arrangement
-    
-    @payment_arrangement.setter
-    def payment_arrangement(self, arrangement: PaymentArrangement):
-        if not isinstance(arrangement, PaymentArrangement):
-            raise ValueError("Invalid payment arrangement!")
-        self.__payment_arrangement = arrangement
-        
-    @property
     def payment_status(self) -> bool:
         return self.__payment_status
     
+    @property
+    def payment_record(self) -> PaymentRecord:
+        return self.__payment_record
     
-
+    @property
+    def manifest(self) -> list[str]:
+        return self.__manifest.copy()
     
-        
+    ## Methods
+    def pay(self, transaction_ID: str, method: PaymentMethod) -> None:        
+        if verify_transaction(transaction_ID, self.amount):
+            self.__payment_status = True
+            self.__payment_record = PaymentRecord(transaction_ID, method)
+        else:
+            print("Error, The transaction ID provided is invalid!")
+            
+    def verify_payment(self) -> bool:
+        transaction_ID = self.payment_record.transaction
+        return verify_transaction(transaction_ID, self.amount)
     
+    def add_item(self, order: Order) -> None:
+        self.__manifest.append(order.ID)
+        self.__amount += order.fee
