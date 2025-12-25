7b1f964cf10faf68d064d81dc65f0e9009a7f547
Samuel Lai <lai.sz.jen@gmail.com>
2025-12-25 13:59:41 +0800

    Add Location and Vehicle Class.

---
+++ Utility/Location.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 25 13:08:25 2025
+
+@author: laisz
+"""
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Order import Order
+
+class Location(ABC):
+    """
+    Attributes:
+        address (str): the address of the location
+        
+    Methods:
+        -
+    """
+    def __init__(self, address: str):
+        self._address = address
+        
+    @property
+    def address(self) -> str:
+        return self._address
+    
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+    
+class Destination(Location):
+    """
+    The final destination of a delivery.
+    """
+    def __str__(self) -> str:
+        return self.address
+    
+
+class Repository(Location):
+    """
+    Attributes:
+        address (str): the address of the location
+        name(str): the name of the repository
+        inventory(set[Order]): a list of cargo at the repository
+    
+    Methods:
+        receive(*orders: Order)
+        ship(*orders: Order)
+    """
+    def __init__(self, address: str, name: str):
+        super().__init__(address)
+        self._name = name
+        self._inventory = set()
+        
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def inventory(self) -> set[Order]:
+        return self._inventory.copy()
+    
+    
+    ## Methods
+    def receive(self, *orders: Order) -> None:
+        self._inventory |= set(orders)
+        
+    def ship(self, *orders: Order) -> None:
+        self._inventory -= set(orders)
+        
+    def __str__(self) -> str:
+        return self.name
---
+++ Utility/Vehicle.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 25 12:44:55 2025
+
+@author: laisz
+"""
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Order import Order
+
+class Vihecle(ABC):
+    """
+    Attributes:
+        license_plate (str): The license plate number of the vehicle
+        cargo (set[Order]): A list of cargo that is on the vehicle
+        
+    Methods:
+        pick_up(*order): put the cargos onto the vehicle
+        deliver(order): take the cargo off the vehicle, and return what is 
+                        removed as a set
+    """
+    def __init__(self, plate: str):
+        self._license_plate = plate
+        self._cargo = set()
+        
+    @property
+    def license_plate(self) -> str:
+        return self._license_plate
+    
+    @property
+    def cargo(self) -> set[Order]:
+        return self._cargo.copy()
+    
+    ## Methods
+    def pick_up(self, *orders: Order) -> None:
+        self._cargo |= set(orders)
+        
+    def deliver(self, order: Order) -> Order:
+        self._cargo.remove(order)
+    
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+    
+    
+class Minivan(Vihecle):
+    def __str__(self) -> str:
+        return f"minivan ({self.license_plate})"
+
+class MiniTruck(Vihecle):
+    def __str__(self) -> str:
+        return f"mini truck ({self.license_plate})"
+
+class Truck(Vihecle):
+    def __str__(self) -> str:
+        return f"truck ({self.license_plate})"
+
+if __name__ == "__main__":
+    car = Truck("ABC-1234")
+    print(car)
+    
+        
